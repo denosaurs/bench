c0bfed1c37367de9e64fcad95817afd54ffc4062
Qu4k <pippo.reds@gmail.com>
2020-09-20 17:44:20 +0200

    chore: bump dependencies

--- _bench/deps.ts
+++ _bench/deps.ts
@@ -1,14 +1,14 @@
-import { EOL as eol } from "https://deno.land/std@0.68.0/fs/eol.ts";
-export { SEP } from "https://deno.land/std@0.65.0/path/separator.ts";
-export { encode } from "https://deno.land/std@0.68.0/encoding/utf8.ts";
+import { EOL as eol } from "https://deno.land/std@0.70.0/fs/eol.ts";
+export { SEP } from "https://deno.land/std@0.70.0/path/separator.ts";
+export { encode } from "https://deno.land/std@0.70.0/encoding/utf8.ts";
 
-export { join } from "https://deno.land/std@0.68.0/path/mod.ts";
+export { join } from "https://deno.land/std@0.70.0/path/mod.ts";
 export {
   parse,
   stringify,
-} from "https://deno.land/std@0.68.0/encoding/yaml.ts";
+} from "https://deno.land/std@0.70.0/encoding/yaml.ts";
 export {
   sentenceCase,
-} from "https://deno.land/x/gutenberg@0.1.1/case/sentence_case/mod.ts";
+} from "https://deno.land/x/gutenberg@0.1.4/case/sentence_case/mod.ts";
 
 export const EOL = Deno.build.os === "windows" ? eol.CRLF : eol.LF;
