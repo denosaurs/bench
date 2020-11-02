9bd3fd9cda1c36a6cd72383d60ed5d5f9748a2f5
eliassjogreen <eliassjogreen1@gmail.com>
2020-11-02 14:11:58 +0100

    chore: bump deps

--- _bench/deps.ts
+++ _bench/deps.ts
@@ -1,12 +1,12 @@
-import { EOL as eol } from "https://deno.land/std@0.75.0/fs/eol.ts";
-export { SEP } from "https://deno.land/std@0.75.0/path/separator.ts";
-export { encode } from "https://deno.land/std@0.75.0/encoding/utf8.ts";
+import { EOL as eol } from "https://deno.land/std@0.76.0/fs/eol.ts";
+export { SEP } from "https://deno.land/std@0.76.0/path/separator.ts";
+export { encode } from "https://deno.land/std@0.76.0/encoding/utf8.ts";
 
-export { join } from "https://deno.land/std@0.75.0/path/mod.ts";
+export { join } from "https://deno.land/std@0.76.0/path/mod.ts";
 export {
   parse,
   stringify,
-} from "https://deno.land/std@0.75.0/encoding/yaml.ts";
+} from "https://deno.land/std@0.76.0/encoding/yaml.ts";
 export {
   sentenceCase,
 } from "https://deno.land/x/gutenberg@0.1.5/case/sentence_case/mod.ts";
--- deno/bare.ts
+++ deno/bare.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.75.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.76.0/http/server.ts";
 const s = serve({ port: 8000 });
 for await (const req of s) {
   req.respond({ body: "Hello World\n" });
