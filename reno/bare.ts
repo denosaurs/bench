53e3d0b621db14b845a41e95177a230a596cf17d
James Wright <hi@james.engineering>
2020-12-18 12:51:57 +0000

    Fixes routes definition

--- reno/bare.ts
+++ reno/bare.ts
@@ -1,12 +1,19 @@
 import { listenAndServe } from "https://deno.land/std/http/server.ts";
-import { createRouter, textResponse } from "https://deno.land/x/reno/reno/mod.ts";
+
+import {
+  createRouteMap,
+  createRouter,
+  textResponse,
+} from "https://deno.land/x/reno/reno/mod.ts";
 
 const BINDING = ":8000";
 
-const router = createRouter([
+const routes = createRouteMap([
   ["/", () => textResponse("Hello, Bench!")],
 ]);
 
+const router = createRouter(routes);
+
 await listenAndServe(BINDING, async (req) => {
-    req.respond(await router(req));
+  req.respond(await router(req));
 });
