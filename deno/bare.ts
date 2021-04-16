0249b391f4b495562c30d58d24891e895986557b
eliassjogreen <eliassjogreen1@gmail.com>
2021-04-16 23:13:03 +0200

    fix?: await all promises when using deno native http

--- deno/bare.ts
+++ deno/bare.ts
@@ -1,9 +1,16 @@
 const body = new TextEncoder().encode("Hello, Bench!");
 
+const p1 = [];
+
 for await (const conn of Deno.listen({ port: 8000 })) {
+  p1.push(
   (async () => {
+    const p2 = [];
     for await (const { respondWith } of Deno.serveHttp(conn)) {
-      respondWith(new Response(body));
+      p2.push(respondWith(new Response(body)));
     }
-  })();
+    await Promise.all(p2);
+  })());
 }
+
+await Promise.all(p1);
