d1668036b49fb012776ca2ce6e89c31efffcf61e
eliassjogreen <eliassjogreen1@gmail.com>
2021-04-16 21:13:22 +0200

    chore: rebuild

--- _bench/deps.ts
+++ _bench/deps.ts
@@ -1,6 +1,5 @@
 import { EOL as eol } from "https://deno.land/std@0.93.0/fs/eol.ts";
 export { SEP } from "https://deno.land/std@0.93.0/path/separator.ts";
-export { encode } from "https://deno.land/std@0.93.0/encoding/utf8.ts";
 
 export { join } from "https://deno.land/std@0.93.0/path/mod.ts";
 export {
