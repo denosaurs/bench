38543df89902ad1b16f9ebd30c3382db8cb6ca82
eliassjogreen <eliassjogreen1@gmail.com>
2021-04-29 00:40:03 +0200

    feat: dont await all promises in raw deno to increase performance maybe

--- deno/bare.ts
+++ deno/bare.ts
@@ -1,17 +1,9 @@
 const body = new TextEncoder().encode("Hello, Bench!");
 
-const p1 = [];
-
 for await (const conn of Deno.listen({ port: 8000 })) {
-  p1.push(
-    (async () => {
-      const p2 = [];
-      for await (const { respondWith } of Deno.serveHttp(conn)) {
-        p2.push(respondWith(new Response(body)));
-      }
-      await Promise.all(p2);
-    })(),
-  );
+  (async () => {
+    for await (const { respondWith } of Deno.serveHttp(conn)) {
+      respondWith(new Response(body));
+    }
+  })();
 }
-
-await Promise.all(p1);
