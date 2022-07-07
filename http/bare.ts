ac7bd023a08b4f281f135fa2d541eb219e101891
eliassjogreen <eliassjogreen1@gmail.com>
2022-07-07 19:29:35 +0200

    fix: attempt to fix fast and std/http

--- http/bare.ts
+++ http/bare.ts
@@ -1,6 +1,3 @@
 import { serve } from "https://deno.land/std/http/server.ts";
 
-const s = serve({ port: 8000 });
-for await (const req of s) {
-  req.respond({ body: "Hello, Bench!" });
-}
+serve(() => new Response("Hello, Bench!"), { port: 8000 });
