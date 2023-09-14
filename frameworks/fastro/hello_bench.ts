b7d815cb01382340061ef032813903b502827d6e
ynwd <10122431+ynwd@users.noreply.github.com>
2023-09-14 16:30:17 +0700

    Update hello_bench.ts

--- frameworks/fastro/hello_bench.ts
+++ frameworks/fastro/hello_bench.ts
@@ -2,6 +2,6 @@ import fastro from "https://deno.land/x/fastro/mod.ts";
 
 const f = new fastro();
 
-f.get("/", () => "Hello, World!");
+f.get("/", () => "Hello, Bench!");
 
 await f.serve({ port: 8000 });
